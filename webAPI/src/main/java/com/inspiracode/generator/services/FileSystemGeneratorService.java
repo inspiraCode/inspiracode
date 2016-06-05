2a70858d2813775ef69844f21622d846f8fe83f9
Alfredo Pacheco <apacheco@capsonic.com>
2016-06-04 18:55:44 -0600

    -Running mvn from app.

--- webAPI/src/main/java/com/inspiracode/generator/services/FileSystemGeneratorService.java
+++ webAPI/src/main/java/com/inspiracode/generator/services/FileSystemGeneratorService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,26 +20,34 @@ public class FileSystemGeneratorService implements GeneratorService {
 
 	@Value("${fs.destination}")
 	private String targetDirectory;
+	
+	@Value("${MAVEN_COMMAND}")
+	private String mavenCMD;
 
 	@Override
 	public void generate(Djson djson) throws GeneratorServiceException {
 
 		createDirectory(targetDirectory);
-		String backendDirectory = targetDirectory + "/backend";
+		String backendDirectory = targetDirectory + "backend";
 		createDirectory(backendDirectory);
 
 		String groupId = String.format("-DgroupId=com.%s.%s", Utils.ToVariable(djson.getTargetCompany()),
 				Utils.ToVariable(djson.getProjectName()));
 		String artifactId = String.format("-DartifactId=%s", Utils.ToVariable(djson.getProjectName()));
-		String[] mvnParameters = { "archetype:generate", groupId, artifactId, "-DinteractiveMode=false" };
-		runCommand(backendDirectory, "mvn", mvnParameters);
+		String[] mvnParameters = {"-B", "archetype:generate", "-DarchetypeGroupId=org.apache.maven.archetypes", groupId, artifactId, "-DinteractiveMode=false" };
+		runCommand(backendDirectory, mavenCMD, mvnParameters);
 	}
 
 	private void runCommand(String targetDirectory, String command, String... args) throws GeneratorServiceException {
 		ProcessBuilder pb = new ProcessBuilder(command);
+		pb.command().addAll(Arrays.asList(args));
+		
 		pb.directory(new File(targetDirectory));
+		//TODO Configure Log destination
+		pb.redirectOutput(new File("c:\\temp\\Maven.log"));
 		try {
 			pb.start();
+			//TODO evaluate process result
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new GeneratorServiceException("Unable to execute command: " + command + ", " + e.getMessage());
