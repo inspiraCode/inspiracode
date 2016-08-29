6a436109951f5353a150e6564f694cf0e7a4c830
Alfredo Pacheco <apacheco@capsonic.com>
2016-08-28 19:58:56 -0600

    -Using CodeMirror third-party library.

--- frontend/main/app/scripts/codemirrorWidget.js
+++ frontend/main/app/scripts/codemirrorWidget.js
@@ -1,5 +1,15 @@
+var theCodeMirror;
+var codeMirrorWidgetConfig = function(element, init) {
+    if (!init) {
+        theCodeMirror = CodeMirror(element, {
+            mode: 'javascript',
+            value: "function myScript(){\n\treturn 100;\n}\n",
+        });
+    }
+}
+
 var codeMirrorWidget = {
     view: function(ctrl) {
-        return m('.codeMirrorWidget', 'code mirror widget');
+        return m('.codeMirrorWidget', { config: codeMirrorWidgetConfig }, '');
     }
 };
